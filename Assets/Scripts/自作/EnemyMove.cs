c1a3d976e1feff7c83ad1281ee4d602af47f989f
PG22-01 <>
2023-10-03 13:00:15 +0900

    エネミー調整

--- Assets/Scripts/自作/EnemyMove.cs
+++ Assets/Scripts/自作/EnemyMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    public float stopYPosition = 1.5f;
+    public float stopYPosition = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
